dc662bd594c626d404915f28454c2fd6ec494aac
Shamim Keshani <sh.keshani@gmail.com>
2024-04-27 14:52:10 +0330

    disable caret when not editing. auto format.

--- src/components/GoalNoteItem.tsx
+++ src/components/GoalNoteItem.tsx
@@ -22,30 +22,30 @@ import ContentCopyIcon from '@mui/icons-material/ContentCopy';
 import { getDirection } from "./../utilities.tsx"
 
 
-export default function GoalNoteItem({type, id, text, done, modifiable, editing, selected, onSubmit, onEdit, onSelect, onDelete, onCancel, onToggle, onFocusLeave}) {
+export default function GoalNoteItem({ type, id, text, done, modifiable, editing, selected, onSubmit, onEdit, onSelect, onDelete, onCancel, onToggle, onFocusLeave }) {
 
   const [editingText, setEditingText] = useState(text);
   const [dir, setDir] = useState('rtl');
   const [hovered, setHovered] = useState(false);
 
   useEffect(() => {
-      if (editing) {
-        setEditingText(text);
-      }
-    }, [editing]);
+    if (editing) {
+      setEditingText(text);
+    }
+  }, [editing]);
 
   useEffect(() => {
-      if (editing) {
-        setDir(getDirection(editingText));
-      } else {
-        setDir(getDirection(text));
-      }
-    }, [text, editingText, editing]);
+    if (editing) {
+      setDir(getDirection(editingText));
+    } else {
+      setDir(getDirection(text));
+    }
+  }, [text, editingText, editing]);
 
   const onFocus = () => { }
 
   const onBlur = () => {
-    onFocusLeave({ id:id, text: editingText});
+    onFocusLeave({ id: id, text: editingText });
   }
 
   const handleKeyDown = (event) => {
@@ -56,134 +56,137 @@ export default function GoalNoteItem({type, id, text, done, modifiable, editing,
       }
     } else if (editing && event.key === 'Enter') {
       if (editingText == "") onCancel(id);
-      else onSubmit({ id:id, text: editingText, keyboard_submit: true});
+      else onSubmit({ id: id, text: editingText, keyboard_submit: true });
       setEditingText("");
     } else if (editing && event.key === 'Escape') {
       onCancel(id);
       setEditingText("");
-    } else {}
+    } else { }
   };
 
   const onCheckBoxChanged = () => {
-      onToggle(id);
+    onToggle(id);
   }
 
   return (
-  <Box
-    dir="rtl"
-    id={id}
-    className="goal_note_item"
-    onMouseEnter={() => {setHovered(true);}}
-    onMouseLeave={() => {setHovered(false);}}
-    sx={{
-      borderRight: selected ? '4px solid darkorchid' : hovered ? '4px solid aliceblue' : '4px solid white',
-      backgroundColor: selected ? 'lavender': hovered ? 'aliceblue' : 'white',
-    }}
-  >
-    <Stack
-      direction="row"
-      alignItems="center"
-      justifyContent="center"
-      spacing={1}
+    <Box
+      dir="rtl"
+      id={id}
+      className="goal_note_item"
+      onMouseEnter={() => { setHovered(true); }}
+      onMouseLeave={() => { setHovered(false); }}
+      sx={{
+        borderRight: selected ? '4px solid darkorchid' : hovered ? '4px solid aliceblue' : '4px solid white',
+        backgroundColor: selected ? 'lavender' : hovered ? 'aliceblue' : 'white',
+      }}
     >
-      {(type == 'Goal') &&
-        <Checkbox
-          checked={done}
-          onChange={onCheckBoxChanged}
-          size="small"
-        />
-      }
-      {(type == 'Note') &&
-        <IconButton aria-label="note" size="small" disabled color="secondary" >
-          <ChatIcon color="text.default" />
-        </IconButton>
-      }
-      {editing &&
-        <>
-        <TextField
-          dir={dir}
-          variant="outlined"
-          size="small"
-          inputProps={{style: {fontSize: (type == 'Note')?"0.85em":"1em"}}}
-          multiline={(type == 'Note')}
-          maxRows={(type == 'Note')?40:1}
-          fullWidth
-          autoFocus
-          onFocus={onFocus}
-          onBlur={onBlur}
-          value={editingText}
-          onKeyDown={handleKeyDown}
-          onChange={(e) => setEditingText(e.currentTarget.value)}
-        />
-        <IconButton
-          aria-label="ok"
-          size="small"
-          color="success"
-          onClick={() => {
-            onSubmit({ id: id, text: editingText });
-            setEditingText("");
-          }}
-        >
-          <CheckCircleIcon fontSize="small"/>
-        </IconButton>
-        <IconButton
-          aria-label="cancel"
-          size="small"
-          color="default"
-          onClick={() => {
-            onCancel(id);
-            setEditingText("");
-          }}
-        >
-          <CancelIcon fontSize="small"/>
-        </IconButton>
-        </>
-      }
-      {!editing &&
-        <>
-        <InputBase
-          dir={dir}
-          size="small"
-          inputProps={{style: {
-            fontSize: (type == 'Note')?"0.9em":"1em",
-            fontWeight:  (type == 'Note')?300:400,
-          }}}
-          multiline={(type == 'Note')}
-          maxRows={(type == 'Note')?40:1}
-          fullWidth
-          value={text}
-          onMouseDown={() => {onSelect(id);}}
-        />
-        <IconButton
-          aria-label="copy"
-          size="small"
-          color="secondary"
-          onClick={() => {navigator.clipboard.writeText(text)}}
-        >
-          <ContentCopyIcon fontSize="small"/>
-        </IconButton>
-        <IconButton
-          aria-label="edit"
-          size="small"
-          color="primary"
-          onClick={() => {
-            onEdit(id);
-          }}
-        >
-          <EditIcon fontSize="small"/>
-        </IconButton>
-        <IconButton
-          aria-label="delete"
-          size="small"
-          color="error"
-          onClick={() => {onDelete(id)}}
-        >
-          <DeleteIcon fontSize="small"/>
-        </IconButton>
-        </>
-      }
-    </Stack>
-  </Box>
+      <Stack
+        direction="row"
+        alignItems="center"
+        justifyContent="center"
+        spacing={1}
+      >
+        {(type == 'Goal') &&
+          <Checkbox
+            checked={done}
+            onChange={onCheckBoxChanged}
+            size="small"
+          />
+        }
+        {(type == 'Note') &&
+          <IconButton aria-label="note" size="small" disabled color="secondary" >
+            <ChatIcon color="text.default" />
+          </IconButton>
+        }
+        {editing &&
+          <>
+            <TextField
+              dir={dir}
+              variant="outlined"
+              size="small"
+              inputProps={{ style: { fontSize: (type == 'Note') ? "0.85em" : "1em" } }}
+              multiline={(type == 'Note')}
+              maxRows={(type == 'Note') ? 40 : 1}
+              fullWidth
+              autoFocus
+              onFocus={onFocus}
+              onBlur={onBlur}
+              value={editingText}
+              onKeyDown={handleKeyDown}
+              onChange={(e) => setEditingText(e.currentTarget.value)}
+            />
+            <IconButton
+              aria-label="ok"
+              size="small"
+              color="success"
+              onClick={() => {
+                onSubmit({ id: id, text: editingText });
+                setEditingText("");
+              }}
+            >
+              <CheckCircleIcon fontSize="small" />
+            </IconButton>
+            <IconButton
+              aria-label="cancel"
+              size="small"
+              color="default"
+              onClick={() => {
+                onCancel(id);
+                setEditingText("");
+              }}
+            >
+              <CancelIcon fontSize="small" />
+            </IconButton>
+          </>
+        }
+        {!editing &&
+          <>
+            <InputBase
+              dir={dir}
+              size="small"
+              inputProps={{
+                style: {
+                  fontSize: (type == 'Note') ? "0.9em" : "1em",
+                  fontWeight: (type == 'Note') ? 300 : 400,
+                  caretColor: 'transparent',
+                }
+              }}
+              multiline={(type == 'Note')}
+              maxRows={(type == 'Note') ? 40 : 1}
+              fullWidth
+              value={text}
+              onMouseDown={() => { onSelect(id); }}
+            />
+            <IconButton
+              aria-label="copy"
+              size="small"
+              color="secondary"
+              onClick={() => { navigator.clipboard.writeText(text) }}
+            >
+              <ContentCopyIcon fontSize="small" />
+            </IconButton>
+            <IconButton
+              aria-label="edit"
+              size="small"
+              color="primary"
+              onClick={() => {
+                onEdit(id);
+              }}
+            >
+              <EditIcon fontSize="small" />
+            </IconButton>
+            <IconButton
+              aria-label="delete"
+              size="small"
+              color="error"
+              onClick={() => { onDelete(id) }}
+            >
+              <DeleteIcon fontSize="small" />
+            </IconButton>
+          </>
+        }
+      </Stack>
+    </Box>
   );
 }
 
