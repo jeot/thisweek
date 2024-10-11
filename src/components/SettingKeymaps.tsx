89dc119a903684b0318e7dad0bec9257f2e418aa
Shamim Keshani <sh.keshani@gmail.com>
2024-10-11 21:58:31 +0330

    feat: display keymaps in setting page

--- src/components/SettingKeymaps.tsx
+++ src/components/SettingKeymaps.tsx
@@ -1,5 +1,6 @@
 import { Typography } from '@mui/material';
 import './styles.css'
+import { KEYMAPS } from '../Keymaps';
 
 export default function SettingAbout() {
 
@@ -7,8 +8,20 @@ export default function SettingAbout() {
     <div className="setting-content-about">
       <Typography variant="h5">Keyboard Mappings</Typography>
       <Typography variant="h5">&nbsp;</Typography>
-      <Typography variant="h6">Alt + Enter: Toggle Full Screen</Typography>
-      <Typography variant="body1" component="p" color="textSecondary">???</Typography>
+      {
+        KEYMAPS.map((item) => {
+          const key = item.keys;
+          const mod = item.mod;
+          const left_str = `${mod} ${mod ? '+' : ''} ${key} : `
+          const right_str = `${item.desc}`;
+          return (
+            <div>
+              <Typography variant="body1" component="span" color="InfoText">{left_str}</Typography>
+              <Typography variant="body1" component="span" color="textSecondary">{right_str}</Typography>
+            </div>
+          );
+        })
+      }
     </div >
   );
 }
