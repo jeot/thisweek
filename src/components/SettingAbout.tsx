c40c68506baa14dda14ef9112e2ed234fe0b1db5
Shamim Keshani <sh.keshani@gmail.com>
2024-10-13 14:54:17 +0330

    it should be your story, not mine

--- src/components/SettingAbout.tsx
+++ src/components/SettingAbout.tsx
@@ -1,26 +1,36 @@
-import { Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
 import { getWeekAppVersion } from '../Globals';
+import { open } from '@tauri-apps/api/shell';
 import './styles.css'
+
+const openLandingPage = () => {
+  open("https://www.google.com/")
+}
+const openGithubPage = () => {
+  open("https://www.github.com/")
+}
 
 export default function SettingAbout() {
   const appVersion = getWeekAppVersion();
 
   return (
-    <div className="setting-content-about">
-      <Typography variant="h5">About</Typography>
+    <div className="settings-content-about">
       <Typography variant="h5">&nbsp;</Typography>
-      <Typography variant="h4" align="center"> <em>WeeksApp</em> </Typography>
+      <Typography variant="h4" align="center"> <em>New Week</em> </Typography>
       <Typography variant="body2" align="center" color="textSecondary">
-        v{appVersion} - <a href="to github page">Source Code</a>
+        v{appVersion} -
+        <Button variant='text' color='info' onClick={() => openGithubPage()} >Source Code</Button>
       </Typography>
       <Typography variant="h5">&nbsp;</Typography>
-      <Typography variant="h5">The Story</Typography>
-      <Typography variant="body1" component="p" color="textSecondary" className="story">
-        I found myself lost in confusion. Each day felt meaningless, as time slipped by without purpose or direction. I lacked the motivation to do anything, unsure of not only what I should do but also why I should even try. <a href="google.com">Continue reading the story...</a></Typography>
-
-      {/*
-      <p>It was August 2022, and I found myself lost in confusion. Each day felt meaningless, as time slipped by without purpose or direction. I lacked the motivation to do anything, unsure of not only what I should do but also why I should even try. This is the story of why did I created WeeksApp. <a href="google.com">Learn More...</a></p>
-      */}
+      <Typography variant="body1" align="center" textAlign='left' component="p" color='gray'>
+        The story starts with you. It ends with you.<br />
+        You take the steps that need to be taken.<br />
+        Some days, it flows; other days, you stumble.<br />
+        But every time you fall, you rise again.<br />
+        And with each new week, you begin again — stronger.<br />
+      </Typography>
+      <Typography variant="h5">&nbsp;</Typography>
+      <Button variant='text' color='info' onClick={() => openLandingPage()} >Go to the website</Button>
     </div >
   );
 }
